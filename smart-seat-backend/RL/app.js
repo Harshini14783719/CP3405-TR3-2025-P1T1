f9981cda477facd4eb06f6069de8b48da5364d11
yutong119 <yutong.ji@my.jcu.edu.au>
2025-11-09 22:32:37 +0800

    Create interface1 to get the data of user status

---
+++ smart-seat-backend/RL/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { getUserBookingState } = require('./db/query');
+const app = express();
+const port = 3000;
+
+// interface 1：provide user status data to the RL environment
+app.get('/api/rl-state', async (req, res) => {
+  const { userId } = req.query;
+  const state = await getUserBookingState(Number(userId));
+  res.json(state);
+});
+
+// Interface 2: Receives the RL decision results and returns them to the front end
